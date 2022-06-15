f3debf02989aae526dcfb1914b2e9cb452201843
Arthur <arturiopr1994@gmail.com>
2022-06-15 22:08:48 +0300

    4

--- JS Nativ/main.js
+++ JS Nativ/main.js
@@ -110,11 +110,106 @@
 // console.log(students.filter(st => st.scores > 100))
 //
 
-// 3 занятие
+// 4 занятие
+
+const todoListID_1='aaa'
+const todoListID_2='bbb'
 
 
+const todoList = [
+    {
+        id: todoListID_1,
+        title: "What to learn",
+        filter: "all",
+
+    },    {
+        id: todoListID_2,
+        title: "What to buy",
+        filter: "all",
+
+    },
+]
+
+const tasks = {
+    [todoListID_1]:[
+        {id:12, title: "JS", isDone:false},
+        {id:13, title: "TS", isDone:false},
+        {id:14, title: "HTML", isDone:false},
+    ],
+    [todoListID_2]:[
+        {id:22, title: "Milk", isDone:false},
+        {id:23, title: "Cheese", isDone:false},
+        {id:24, title: "Ice-cream", isDone:false},
+    ]
+}
+
+//reduce
+
+const nums = [10, 20, 30] // sum
+console.log(nums.reduce((acc,el)=>{
+    acc = acc +el
+    return acc
+}, 0))
 
 
+console.log(nums.reduce((acc, el)=>{
+    if(acc > el){
+        return acc
+    } else{
+        return el
+    }
+}))
 
+let students = [
+    {
+        id:1,
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85
+    },
+    {
+        id:2,
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 89
+    },
+    {
+        id:3,
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        id:4,
+        name: "John",
+        age: 23,
+        isMarried: false,
+        scores: 100
+    }
+];
 
+console.log(students.reduce((acc, el)=> acc.scores>el.scores ? acc :el))
 
+console.log(students.reduce((acc, el)=>acc+el.scores, 0))
+console.log(students.reduce((acc, el)=>{
+    acc.push({...el, scores: el.scores+10})
+    return acc
+},[]))
+
+const st = {
+    1:{
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85
+    }
+}
+
+console.log(students.reduce((acc, ell)=>{
+    acc[ell.id]={...ell}
+    delete acc[ell.id].id
+    return acc
+},{}))
