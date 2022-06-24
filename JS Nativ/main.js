dce00a49cc8a707b06045f0112281df95d13504e
Arthur <arturiopr1994@gmail.com>
2022-06-24 12:51:51 +0300

    5 Sort

--- JS Nativ/main.js
+++ JS Nativ/main.js
@@ -112,53 +112,128 @@
 
 // 4 занятие
 
-const todoListID_1='aaa'
-const todoListID_2='bbb'
+// const todoListID_1='aaa'
+// const todoListID_2='bbb'
+//
+//
+// const todoList = [
+//     {
+//         id: todoListID_1,
+//         title: "What to learn",
+//         filter: "all",
+//
+//     },    {
+//         id: todoListID_2,
+//         title: "What to buy",
+//         filter: "all",
+//
+//     },
+// ]
+//
+// const tasks = {
+//     [todoListID_1]:[
+//         {id:12, title: "JS", isDone:false},
+//         {id:13, title: "TS", isDone:false},
+//         {id:14, title: "HTML", isDone:false},
+//     ],
+//     [todoListID_2]:[
+//         {id:22, title: "Milk", isDone:false},
+//         {id:23, title: "Cheese", isDone:false},
+//         {id:24, title: "Ice-cream", isDone:false},
+//     ]
+// }
+//
+// //reduce
+//
+// const nums = [10, 20, 30] // sum
+// console.log(nums.reduce((acc,el)=>{
+//     acc = acc +el
+//     return acc
+// }, 0))
+//
+//
+// console.log(nums.reduce((acc, el)=>{
+//     if(acc > el){
+//         return acc
+//     } else{
+//         return el
+//     }
+// }))
+//
+// let students = [
+//     {
+//         id:1,
+//         name: "Bob",
+//         age: 22,
+//         isMarried: true,
+//         scores: 85
+//     },
+//     {
+//         id:2,
+//         name: "Alex",
+//         age: 21,
+//         isMarried: true,
+//         scores: 89
+//     },
+//     {
+//         id:3,
+//         name: "Nick",
+//         age: 20,
+//         isMarried: false,
+//         scores: 120
+//     },
+//     {
+//         id:4,
+//         name: "John",
+//         age: 23,
+//         isMarried: false,
+//         scores: 100
+//     }
+// ];
+//
+// console.log(students.reduce((acc, el)=> acc.scores>el.scores ? acc :el))
+//
+// console.log(students.reduce((acc, el)=>acc+el.scores, 0))
+// console.log(students.reduce((acc, el)=>{
+//     acc.push({...el, scores: el.scores+10})
+//     return acc
+// },[]))
+//
+// const st = {
+//     1:{
+//         name: "Bob",
+//         age: 22,
+//         isMarried: true,
+//         scores: 85
+//     }
+// }
+//
+// console.log(students.reduce((acc, ell)=>{
+//     acc[ell.id]={...ell}
+//     delete acc[ell.id].id
+//     return acc
+// },{}))
 
 
-const todoList = [
-    {
-        id: todoListID_1,
-        title: "What to learn",
-        filter: "all",
+//5 занятие
 
-    },    {
-        id: todoListID_2,
-        title: "What to buy",
-        filter: "all",
+const names = ["Artur", "Bob", "Vlad", "Alex"]
 
-    },
-]
+console.log(names.sort())
 
-const tasks = {
-    [todoListID_1]:[
-        {id:12, title: "JS", isDone:false},
-        {id:13, title: "TS", isDone:false},
-        {id:14, title: "HTML", isDone:false},
-    ],
-    [todoListID_2]:[
-        {id:22, title: "Milk", isDone:false},
-        {id:23, title: "Cheese", isDone:false},
-        {id:24, title: "Ice-cream", isDone:false},
-    ]
+const number = [100, 99, 665, 1000]
+
+const compFn = (a, b) => {//сортировать по возрастанию
+    if (a > b) {
+        return 10
+    } else {
+        return -10
+    }
 }
 
-//reduce
+console.log(number.sort(compFn))
+console.log(number.sort((a, b) => a-b))
 
-const nums = [10, 20, 30] // sum
-console.log(nums.reduce((acc,el)=>{
-    acc = acc +el
-    return acc
-}, 0))
-
-
-console.log(nums.reduce((acc, el)=>{
-    if(acc > el){
-        return acc
-    } else{
-        return el
-    }
-}))
 
 let students = [
     {
@@ -191,25 +266,22 @@ let students = [
     }
 ];
 
-console.log(students.reduce((acc, el)=> acc.scores>el.scores ? acc :el))
+console.log(students.sort((a, b)=>b.scores-a.scores ))
+console.log(students.sort((a, b)=>b.scores-a.scores ).reverse())
+console.log(students.sort((a, b)=>a.name.localeCompare(b.name)))
+console.log(students.sort((a, b)=>a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1))
 
-console.log(students.reduce((acc, el)=>acc+el.scores, 0))
-console.log(students.reduce((acc, el)=>{
-    acc.push({...el, scores: el.scores+10})
-    return acc
-},[]))
 
-const st = {
-    1:{
-        name: "Bob",
-        age: 22,
-        isMarried: true,
-        scores: 85
+// bubble sort
+
+const nums = [13, 45, 67, 12, 99, 87]
+// по возрастанию
+for (let j = 0; j < nums.length; j++) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > nums[i + 1]) {
+            let temp = nums[i]
+            nums[i] = nums[i + 1]
+            nums[i + 1] = temp
+        }
     }
 }
-
-console.log(students.reduce((acc, ell)=>{
-    acc[ell.id]={...ell}
-    delete acc[ell.id].id
-    return acc
-},{}))
