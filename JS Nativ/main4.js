7ade015291997e857702b1fe73807306baaf613c
Arthur <arturiopr1994@gmail.com>
2022-07-01 17:32:08 +0300

    6

---
+++ JS Nativ/main4.js
@@ -0,0 +1,162 @@
+console.log("Heloo")
+
+const students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 90,
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    },
+    {
+        name: "Helen",
+        age: 20,
+        isMarried: false,
+        scores: 110
+    },
+    {
+        name: "Ann",
+        age: 20,
+        isMarried: false,
+        scores: 105
+    },
+];
+
+const user = {
+    name: "Bob",
+    age: 23,
+    friends: ["Alex", "Nick", "John"]
+}
+
+//1. Создайте поверхностную копию объекта user
+let copyUser = {...user};
+
+//Проверка:
+// console.log(user===copyUser)- false
+// console.log(user.friends===copyUser.friends)- true
+
+//2. Полная (глубокая) копия объекта user
+let deepCopyUser = {...user, friends: [...user.friends]};
+
+//Проверка:
+// console.log(user===deepCopyUser) - true
+// console.log(user.friends===deepCopyUser.friends) - true
+
+//3. Поверхностная копия массива students
+let copyStudents= [...students]
+
+//Проверка:
+// console.log(код проверки написать самостоятельно ) - что должно быть в консоли?
+// console.log(код проверки написать самостоятельно) - что должно быть в консоли?
+// console.log(код проверки написать самостоятельно) - что должно быть в консоли?
+// console.log(код проверки написать самостоятельно) - что должно быть в консоли?
+
+//4*. Полная (глубокая) копия массива students (map)
+let deepCopyStudents = students.map(f => ({...f}));
+console.log(deepCopyStudents)
+
+//Проверка:
+// console.log(код проверки написать самостоятельно) - что должно быть в консоли?
+// console.log(код проверки написать самостоятельно) - что должно быть в консоли?
+// console.log(код проверки написать самостоятельно) - что должно быть в консоли?
+// console.log(код проверки написать самостоятельно) - что должно быть в консоли?
+
+// NB!!! Далее все преобразования выполняем не модифицируя исходный массив students
+// Вывод результатов - в консоль
+
+//5. Отсортируйте копию массива deepCopyStudents по алфавиту (sort)
+let sortedByName = students.sort((a,b)=>a.name > b.name ? 1 : -1);
+console.log(sortedByName);
+
+
+//5a. Отсортируйте deepCopyStudents по успеваемости(лучший идёт первым)(sort)
+let sortedByScores = deepCopyStudents.sort((a,b)=>a.scores > b.scores ? -1 : 1);
+console.log(sortedByScores);
+
+//6. Сформируйте массив студентов, у которых 100 и более баллов (filter)
+let  bestStudents = students.filter(f=>f.scores>=100);
+console.log(bestStudents)
+
+//6a. Получите массив ("вырежьте") из трёх лучших студентов из массива deepCopyStudents (splice)
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
+let topStudents= deepCopyStudents.splice(0,3);
+console.log(topStudents)
+console.log(deepCopyStudents)
+
+//6b. Объедините массивы deepCopyStudents и topStudents так,
+//чтоб сохранился порядок сортировки (spread-оператор || concat)
+let newDeepCopyStudents = [...deepCopyStudents, ...topStudents];
+console.log(newDeepCopyStudents)
+
+
+//7. Сформируйте массив холостых студентов (filter)
+let notMarriedStudents = students.filter(f=>f.isMarried!==true);
+console.log(notMarriedStudents)
+
+//8. Сформируйте массив имён студентов (map)
+let studentsNames = students.map(f=>f.name);
+console.log(studentsNames)
+
+//8a. Сформируйте строку из имён студентов, разделённых
+// - пробелом (join)
+// - запятой (join)
+let namesWithSpace = studentsNames.join(",");
+console.log(namesWithSpace)
+let namesWithComma = studentsNames.join(" ");;
+console.log(namesWithComma)
+
+//9. Добавьте всем студентам свойство "isStudent" со значением true (map)
+let trueStudents = students.map(f=>{
+    return {...f,isStudent: true }
+});
+console.log(trueStudents)
+
+//10. Nick женился. Выполните соответствующие преобразование массива students (map)
+let studentsWithMarriedNick
+console.log(studentsWithMarriedNick)
+
+//11. Найдите студента по имени Ann (find)
+let ann = students.find(st=>st.name==="Ann");
+console.log(ann)
+
+//12. Найдите студента с самым высоким баллом (reduce)
+// - c помощью reduce
+// - *не испльзуя методы массивов и Math.max()*
+let bestStudent = students.reduce((acc, cur)=>acc.scores > cur.scores ? acc : cur);
+console.log(bestStudent)
+
+//13. Найдите сумму баллов всех студентов (reduce)
+let bestStudent = students.reduce((acc, cur)=>acc + cur.scores, 0);
+// И поднимаем руку!!!!
+
+let scoresSum;
+console.log(scoresSum)
+// 14. Д.З.:
+// Напишите функцию addFriends, которая принимает параметром массив students
+// и добавляет в каждому студенту свойство "friends",
+// значением которого является массив имён всех остальных студентов из массива students,
+// за исключением собственного имени студента. Т.е. в друзьях у Боба Боба быть не должно.
+const addFriends = (students) => {
+    //..............................
+}
+console.log(addFriends(students));
+
+
